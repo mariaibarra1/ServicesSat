9250245c4d019bf3e5de83a4f32950ab130fa187
Maria Fernanda Gomez Ibarra <21031723p@gmail.com>
2022-02-18 10:01:25 -0600

    -- innit

---
+++ webserviceSAT/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ServiceReference2;
+
+namespace webserviceSAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference2.ConsultaCFDIServiceClient consulta = new ConsultaCFDIServiceClient();
+            var x = consulta.ConsultaAsync("re=MNM9102019A5&rr=SARL831116NJ5&tt=4337.00&id=614C7879-A448-4499-85A9-483C251BDD12");
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(x.Result.CodigoEstatus );
+            Console.WriteLine(x.Result.Estado);
+            Console.WriteLine(x.Result.EsCancelable);
+            Console.WriteLine(x.Result.EstatusCancelacion);
+
+
+
+ }
+    }
+}
